dfd8c2ee9b384e5adc5bea55b3b65e81d4812f16
BragaMPedro <pedrobmagalhaes95@gmail.com>
2024-03-17 11:10:56 -0300

    update ficha-form.html and ficha-form.js

--- pages/ficha-form/ficha-form.js
+++ pages/ficha-form/ficha-form.js
@@ -8,7 +8,14 @@ function handleSubmit(e) {
    let classe = formData.get("classe");
    let historia = formData.get("historia");
 
-   console.log("Form", { nome, raca, classe, genero, historia });
+   confirm(
+      `Confirme suas informações:\n
+      ${nome}
+      ${genero === "m" ? "Masculino" : genero === "f" ? "Femenino" : "Sem gênero pra mim, obrigado"}
+      ${titlefyValue(raca)}
+      ${titlefyValue(classe)}
+      ${encurtaTexto(historia)}`
+   );
 }
 
 function racaAleatoria() {
@@ -28,13 +35,65 @@ function racaAleatoria() {
 
 function classeAleatoria() {
    let classes = document.getElementById("classe");
-   let classesArray = classes.textContent.match(/\S+/gm);
+   let classesArray = classes.textContent.match(/\S+/gm); //regex 101
    let randomIndex = Math.floor(Math.random() * classesArray.length);
 
    selectValue("classe", classesArray[randomIndex]);
 }
 
+/**
+ * Seleciona o atributo value de um elemento HTML
+ *
+ * @param {string} id ID do elemento que deseja atribuir valor
+ * @param {string} value String value que deseja atribuir ao elemento
+ */
 function selectValue(id, value) {
-   let selectElement = document.getElementById(id);
-   selectElement.value = value.toLowerCase()
+   let selectedElement = document.getElementById(id);
+
+   selectedElement.value = value.toLowerCase();
+}
+
+/**
+ *Verifica se o parametro texto é maior que o length escolhido e retorna a string encurtada.
+ * @param {string} texto texto a ser encurtado
+ * @param {number} length number representando o tamanho limite do parametro texto
+ * @returns {string} Texto encurtado caso passe caso passe do length escolhido.
+ * @default length 25
+ */
+function encurtaTexto(texto, length = 25) {
+   let textoReturn = texto.length > length ? texto.substring(0, length) : texto;
+
+   return textoReturn.concat("...");
+}
+
+/**
+ *
+ * @param {string} value
+ * @returns {string}
+ */
+function titlefyValue(value) {
+   let titlefiedValue = "";
+   let multiPalavra = value.includes("-");
+
+   if (multiPalavra) {
+      let valueArray = value.split("-");
+
+      for (const word of valueArray) {
+         let capitalizedWord = capitalize(word);
+
+         titlefiedValue.concat(capitalizedWord + " ");
+      }
+   } else {
+      titlefiedValue = capitalize(value);
+   }
+
+   titlefiedValue.trimEnd();
+
+   return titlefiedValue;
+}
+
+function capitalize(string){
+   let capitalizedString = string.replace(string.charAt(0), string.charAt(0).toUpperCase());
+
+   return capitalizedString
 }
